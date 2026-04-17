991a2b08fb817fdf8a2a77e2307061c2c0e068f6
Victor Rafael Silva De Andrade <00001128157627sp@aluno.educacao.sp.gov.br>
2026-04-16 21:14:46 -0300

    ihbi

--- server.js
+++ server.js
@@ -1,9 +1,22 @@
 import express from "express"
+import cors from "cors"
+import mysql from "mysql2"
 
 const app = express()
+app.use(cors())
+app.use(express.json())
+
+const db = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "",
+    database: "saude"
+})
 
 app.post("/cadastro", (req, res) => {
-    res.send("oioioio")
+    const { nome, email, senha } = req.body
+
+    const sql = "INSERT INTO usuarios (nome, email, senha) VALUE (?,?,?)"
 })
 
 app.listen(3000, () => {
